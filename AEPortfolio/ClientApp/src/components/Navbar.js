b1c707f643cfa9e5aecd1a67ce90e1b0f34bf7da
Abdullah Ejaz <aejaz3@dxc.com>
2021-07-18 16:12:53 -0500

    Navbar component

---
+++ AEPortfolio/ClientApp/src/components/Navbar.js
@@ -0,0 +1,36 @@
+﻿import React from 'react'
+import logo from './Images/developer.png'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo...." /></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarScroll">
+                    <ul className="navbar-nav ml-auto my-2 my-lg-0 navbar-nav-scroll" styles="--bs-scroll-height: 100px;">
+                        <li className="nav-item">
+                            <a className="nav-link" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Experience</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Education</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Projects</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
